4549f07225147c37229c2d367a4fcfe42d95985f
gfike19 <gfike19@gmail.com>
2022-04-07 17:52:10 -0500

    completed part 2 of walkthrough

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -8,12 +8,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
+
+    private final PostRepository postRepository;
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
     private final String[] gadgets = {
@@ -21,12 +22,15 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
-    public DatabaseLoader() {
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         IntStream.range(0,40).forEach(i->{
+            postRepository.saveAll(randomPosts);
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
 
